e0cfa5f5e560975ecb27e4c4561397c4bca7d7e4
arellanoc14 <46659181+arellanoc14@users.noreply.github.com>
2019-06-18 15:22:39 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,6 @@ import axios from 'axios';
 import Business from './business';
 import SearchBar from './searchBar';
 import {getYelp} from './httpService';
-const apiKey = "K4FRgQNCw2Zb4-HkoRtN0JmDDcBy-TYh5dLOdE_lSZLDSU0KenCeo7WHCsPCrUWkZcrTCfyw6jczTZ-CPTBV4yxVPj8hlnI4Cwu2oB1mqhmHjZJy5MgpgZ29RPYDXXYx";
-const baseUrl = "https://api.yelp.com/v3/businesses/search";
-const herokuapp = 'https://cors-anywhere.herokuapp.com/';
-const config = {
-    headers: {
-      'Authorization': `Bearer ${apiKey}`,
-    },
-    params: {
-      term: 'taco',
-      location: 'PortLand',
-    } 
-};
 class App extends Component {
     state = {
       term:'',
